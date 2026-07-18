41b95cc692ccd4b693861c52193c91a047250296
jonasnys@gmail.com <jonasnys@gmail.com>
2026-07-18 20:46:32 +0200

    Connected wasm to worker

--- islands/workers/testwasm.worker.ts
+++ islands/workers/testwasm.worker.ts
@@ -1,3 +1,6 @@
+import { add } from "../wasm/lib/rs_lib.js";
+console.log("Test WASM-WORKER running", add(33, 55));
+
 self.onmessage = ({ data }) => {
   console.log("Test WASM-WORKER", data);
   self.postMessage("this WASM-WORKER is working");
