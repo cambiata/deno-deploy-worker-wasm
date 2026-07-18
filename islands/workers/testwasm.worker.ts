78003ebbe87cecfa56e95082a975a62f86eb268f
jonasnys@gmail.com <jonasnys@gmail.com>
2026-07-18 21:32:52 +0200

    update

--- islands/workers/testwasm.worker.ts
+++ islands/workers/testwasm.worker.ts
@@ -1,7 +1,16 @@
 import { add } from "../wasm/lib/rs_lib.js";
-console.log("Test WASM-WORKER running", add(33, 55));
+console.log("22 + 44 =", add(22, 44));
+
+// console.log(add);
 
 self.onmessage = ({ data }) => {
   console.log("Test WASM-WORKER", data);
-  self.postMessage("this WASM-WORKER is working");
+  const sum = add(33, 55);
+  // console.log("sum in WASM-WORKER:", sum);
+  self.postMessage("Test WASM-WORKER is working: 33 + 55 = " + sum);
+  // self.postMessage("Test WASM-WORKER is working");
 };
+
+// const sum = (a: number, b: number) => {
+//   return add(a, b);
+// };
